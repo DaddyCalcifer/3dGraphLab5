ff67dbfc5bc3ab554d57854eb7ed3308e07cbc05
Никита Черничкин <nikita.macarov@yandex.ru>
2023-04-05 22:23:49 +0300

    Добавьте файлы проекта.

---
+++ GraphSprite/Form1.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace GraphSprite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer_update = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::GraphSprite.Properties.Resources.land1;
+            this.ClientSize = new System.Drawing.Size(882, 511);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Игра (Лаб. 5)";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer_update;
+    }
+}
+
---
+++ GraphSprite/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphSprite
+{
+    public partial class Form1 : Form
+    {
+        private Rectangle Hero;
+        Image[] run_left,run_right,idle_;
+        private int FrameIndex = 0;
+        readonly string AnimationPath = @"C:\Users\Nikita\Desktop\Animations\";
+        Animation CurrentAnimation = Animation.idle;
+
+        private const int Speed = 7, IdleTick=90, RunTick=55;
+
+        Image background;
+        enum Animation { left, right, idle };
+
+        public Form1()
+        {
+            this.DoubleBuffered = true;
+            InitializeComponent();
+
+            Hero = new Rectangle(new Point((int)this.Width/2,(int)this.Height/2), new Size(77, 100));
+            background = this.BackgroundImage;
+
+            timer_update.Interval = IdleTick;
+            timer_update.Tick += new EventHandler(Update);
+            timer_update.Start();
+
+            this.KeyDown += new KeyEventHandler(KeyDownned);
+            this.KeyUp += new KeyEventHandler(KeyUpped);
+            //
+            String[] pathes;
+            run_left = new Image[8];
+            pathes = System.IO.Directory.GetFiles(AnimationPath + "\\left\\");
+            for(int i=0; i < run_left.Length;i++)
+            {
+                run_left[i] = Image.FromFile(pathes[i]);
+            }
+            //
+            run_right = new Image[8];
+            pathes = System.IO.Directory.GetFiles(AnimationPath + "\\right\\");
+            for (int i = 0; i < run_right.Length; i++)
+            {
+                run_right[i] = Image.FromFile(pathes[i]);
+            }
+            //
+            idle_ = new Image[8];
+            pathes = System.IO.Directory.GetFiles(AnimationPath + "\\idle\\");
+            for (int i = 0; i < idle_.Length; i++)
+            {
+                idle_[i] = Image.FromFile(pathes[i]);
+            }
+        }
+
+        private void KeyUpped(object sender, KeyEventArgs e)
+        {
+            CurrentAnimation = Animation.idle;
+            timer_update.Interval = IdleTick;
+        }
+
+        private void KeyDownned(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode.ToString())
+            {
+                case "S":
+                    CurrentAnimation = Animation.right;
+                    Hero.Y += Speed;
+                    break;
+                case "A":
+                    CurrentAnimation = Animation.left;
+                    Hero.X -= Speed;
+                    break;
+                case "W":
+                    CurrentAnimation = Animation.right;
+                    Hero.Y -= Speed;
+                    break;
+                case "D":
+                    CurrentAnimation = Animation.right;
+                    Hero.X += Speed;
+                    break;
+            }
+            timer_update.Interval = RunTick;
+        }
+
+        private void Update(object sender, EventArgs e)
+        {
+            if (FrameIndex == 8) FrameIndex = 0;
+
+            PlayAnimation();
+
+            if (Hero.X > this.Width + 77)
+                Hero.X = -50;
+            if (Hero.X < -77)
+                Hero.X = this.Width + 50;
+
+            if (Hero.Y > this.Height - 100)
+                Hero.Y = -80;
+            if (Hero.Y < -100)
+                Hero.Y = this.Height - 90;
+
+            FrameIndex++;
+        }
+
+        private void PlayAnimation()
+        {
+            Bitmap bitmap = new Bitmap(this.Width,this.Height);
+            Graphics cf = Graphics.FromImage(bitmap);
+            cf.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            cf.DrawImage(background,new Rectangle(0,0,this.Width,this.Height));
+            switch(CurrentAnimation)
+            {
+                case Animation.left:
+                    cf.DrawImage(run_left[FrameIndex], Hero);
+                    break;
+                case Animation.right:
+                    cf.DrawImage(run_right[FrameIndex], Hero);
+                    break;
+               default:
+                    cf.DrawImage(idle_[FrameIndex], Hero);
+                    break;
+            }
+            
+            this.BackgroundImage = bitmap;
+        }
+    }
+}
